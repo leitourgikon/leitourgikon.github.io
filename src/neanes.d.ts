d4d06563b71cfcbf538ab2b1f19f602c045d7923
dani31x <176744712+dani31x@users.noreply.github.com>
2025-02-21 20:48:36 +0200

    Declare some of Neanes' web components

---
+++ src/neanes.d.ts
@@ -0,0 +1,45 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+
+declare namespace React {
+  namespace JSX {
+    interface IntrinsicElements {
+      'x-a': any
+      'x-bar-s': any
+      'x-dc': any
+      'x-dg': any
+      'x-e': any
+      'x-g': any
+      'x-gg': any
+      'x-i': any
+      'x-ii-ni': any
+      'x-kl': any
+      'x-kl-b': any
+      'x-ko': any
+      'x-ly': any
+      'x-m': any
+      'x-m-delta': any
+      'x-m-delta-aa': any
+      'x-m-di': any
+      'x-m-legetos': any
+      'x-m-nana': any
+      'x-m-ni': any
+      'x-m-ni-high': any
+      'x-m-vou': any
+      'x-mel': any
+      'x-mode-ni': any
+      'x-mode-plagal': any
+      'x-mode-plagal-fourth': any
+      'x-mode-word-echos': any
+      'x-n': any
+      'x-o': any
+      'x-o-i-k': any
+      'x-o2-m': any
+      'x-o3': any
+      'x-o4': any
+      'x-ok': any
+      'x-p': any
+      'x-psi': any
+      'x-var': any
+    }
+  }
+}
