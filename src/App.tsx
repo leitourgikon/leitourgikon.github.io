7a9c67802760ef99401fe281d1b76d960dc43440
dani31x <176744712+dani31x@users.noreply.github.com>
2025-02-22 14:10:38 +0200

    Add a text on the index path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Bibliotheke from './routes/bibliotheke'
+import Home from './routes/home'
 import Layout from './routes/layout'
 import Leitourgia from './routes/leitourgia'
 import Orthros from './routes/orthros'
@@ -9,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route index />
+          <Route index element={<Home />} />
           <Route path="orthros" element={<Orthros />} />
           <Route path="leitourgia" element={<Leitourgia />} />
           <Route path="bibliotheke" element={<Bibliotheke />} />
