33c037d3238415b53071f34b9e8bff6b4c7ec85c
dani31x <176744712+dani31x@users.noreply.github.com>
2025-03-02 22:34:53 +0200

    Add 'It Is Truly Meet' by Gregorios Protopsaltis in third mode

---
+++ src/chants/12.tsx
@@ -0,0 +1,816 @@
+export default function Component() {
+  return (
+    <div>
+      <div className="byz--mode-key">
+        <x-mode-word-echos></x-mode-word-echos>
+        <x-mode-third-nana></x-mode-third-nana>
+        <x-mode-ga></x-mode-ga>
+      </div>
+      <div className="byz--text-box">Григориос Протопсалт</div>
+      <div className="byz--neume-paragraph">
+        <x-dc>Д</x-dc>
+        <x-n>
+          <x-i></x-i>
+          <x-ii-ga className="byz--ii"></x-ii-ga>
+          <x-ly>ос</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o3></x-o3>
+          <x-psi></x-psi>
+          <x-f-agem className="byz--f"></x-f-agem>
+          <x-ly>той</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-ly>но</x-ly>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-ok></x-ok>
+          <x-psi></x-psi>
+          <x-ly>е</x-ly>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-ly>на</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-a></x-a>
+          <x-kl></x-kl>
+          <x-ly>и</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-ly>сти</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-k></x-k>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o></x-o>
+          <x-ly>на</x-ly>
+        </x-n>
+        <x-m>
+          <x-m-ke></x-m-ke>
+          <x-m-alpha-aa></x-m-alpha-aa>
+        </x-m>
+        <x-n>
+          <x-i></x-i>
+          <x-ly>да</x-ly>
+        </x-n>
+        <x-n>
+          <x-i></x-i>
+          <x-ly>об</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-ly>ла</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o></x-o>
+          <x-kl></x-kl>
+          <x-ly>жа</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-ly>ва</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-re></x-re>
+          <x-ly>ме</x-ly>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o></x-o>
+          <x-dipli></x-dipli>
+          <x-ly>Теб</x-ly>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-anti></x-anti>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-a></x-a>
+          <x-kl></x-kl>
+          <x-ly>Бо</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-o-a-k></x-o-a-k>
+          <x-ly>го</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o-a-k></x-o-a-k>
+          <x-g className="byz--g"></x-g>
+          <x-ii-pa className="byz--ii"></x-ii-pa>
+          <x-ly>ро</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-o-i-k></x-o-i-k>
+          <x-g className="byz--g"></x-g>
+          <x-ly>ди</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-var></x-var>
+          <x-i></x-i>
+          <x-ly>ца</x-ly>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-dipli></x-dipli>
+        </x-n>
+        <x-m>
+          <x-m-pa></x-m-pa>
+          <x-m-alpha></x-m-alpha>
+        </x-m>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o3></x-o3>
+          <x-ii-ga className="byz--ii"></x-ii-ga>
+          <x-ly>ви</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-i></x-i>
+          <x-ly>на</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-ly>ги</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-ly>бла</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o></x-o>
+          <x-kl></x-kl>
+          <x-ly>же</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-ly>на</x-ly>
+        </x-n>
+        <x-n>
+          <x-y></x-y>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-a></x-a>
+          <x-ly>и</x-ly>
+        </x-n>
+        <x-n>
+          <x-ko></x-ko>
+          <x-argon className="byz--g"></x-argon>
+        </x-n>
+        <x-n>
+          <x-i></x-i>
+          <x-ly>пре</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-var></x-var>
+          <x-i></x-i>
+          <x-ii-ke className="byz--ii"></x-ii-ke>
+          <x-ly>не</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-di2></x-di2>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-f-hc-pa className="byz--f"></x-f-hc-pa>
+          <x-ly>по</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-k></x-k>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-ok></x-ok>
+          <x-ly>ро</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-var></x-var>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-var></x-var>
+          <x-i></x-i>
+          <x-ly>чна</x-ly>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-apli></x-apli>
+        </x-n>
+        <x-n>
+          <x-i></x-i>
+          <x-ly>и</x-ly>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-p></x-p>
+          <x-kl-b></x-kl-b>
+          <x-f-agem className="byz--f"></x-f-agem>
+          <x-ii-ga className="byz--ii"></x-ii-ga>
+          <x-ly>май</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-a></x-a>
+          <x-ly>ка</x-ly>
+        </x-n>
+        <x-n>
+          <x-k></x-k>
+        </x-n>
+        <x-n>
+          <x-ok></x-ok>
+          <x-ly>на</x-ly>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-var></x-var>
+          <x-a></x-a>
+          <x-ly>на</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-et-c></x-et-c>
+        </x-n>
+        <x-n>
+          <x-o-i-k></x-o-i-k>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-a></x-a>
+          <x-anti></x-anti>
+          <x-apli></x-apli>
+          <x-ii-pa className="byz--ii"></x-ii-pa>
+          <x-ly>ши</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-var></x-var>
+          <x-i></x-i>
+          <x-ly>я</x-ly>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-var></x-var>
+          <x-i></x-i>
+          <x-ly>Бог</x-ly>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-dipli></x-dipli>
+        </x-n>
+        <x-m>
+          <x-m-pa></x-m-pa>
+          <x-m-alpha></x-m-alpha>
+        </x-m>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o3></x-o3>
+          <x-psi></x-psi>
+          <x-ii-ga className="byz--ii"></x-ii-ga>
+          <x-ly>по</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-kl></x-kl>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o></x-o>
+          <x-kl></x-kl>
+          <x-ly>чест</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-anti></x-anti>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-ly>ва</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-i></x-i>
+          <x-kl></x-kl>
+          <x-ly>на</x-ly>
+        </x-n>
+        <x-m>
+          <x-m-ke></x-m-ke>
+          <x-m-alpha-aa></x-m-alpha-aa>
+        </x-m>
+        <x-n>
+          <x-i></x-i>
+          <x-ly>от</x-ly>
+        </x-n>
+        <x-n>
+          <x-ko></x-ko>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o-e-k></x-o-e-k>
+          <x-gd className="byz--g"></x-gd>
+          <x-ly>Хе</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-i></x-i>
+        </x-n>
+        <x-n>
+          <x-re></x-re>
+          <x-ly>ру</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o2></x-o2>
+          <x-psi></x-psi>
+          <x-ly>ви</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-ly>ми</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-kl></x-kl>
+          <x-ly>те</x-ly>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-i></x-i>
+          <x-f-d-ke-b className="byz--f"></x-f-d-ke-b>
+          <x-ii-ke className="byz--ii"></x-ii-ke>
+          <x-ly>и</x-ly>
+        </x-n>
+        <x-n>
+          <x-k></x-k>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-ly>не</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-ly>срав</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-var></x-var>
+          <x-a></x-a>
+          <x-kl></x-kl>
+          <x-ly>ни</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-y></x-y>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-anti></x-anti>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-i></x-i>
+          <x-kl></x-kl>
+          <x-ly>мо</x-ly>
+        </x-n>
+        <x-m>
+          <x-m-ke></x-m-ke>
+          <x-m-alpha-aa></x-m-alpha-aa>
+        </x-m>
+        <x-n>
+          <x-i></x-i>
+          <x-kl></x-kl>
+          <x-ly>по</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o3></x-o3>
+          <x-psi></x-psi>
+          <x-ly>сла</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-a></x-a>
+          <x-ii-pa className="byz--ii"></x-ii-pa>
+          <x-ly>вна</x-ly>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-ly>от</x-ly>
+        </x-n>
+        <x-n>
+          <x-k></x-k>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o></x-o>
+          <x-kl></x-kl>
+          <x-ly>Се</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-o-i-k></x-o-i-k>
+          <x-ly>ра</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-a></x-a>
+          <x-anti></x-anti>
+          <x-apli></x-apli>
+          <x-ly>фи</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-var></x-var>
+          <x-i></x-i>
+          <x-ly>ми</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-var></x-var>
+          <x-i></x-i>
+          <x-ly>те</x-ly>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-dipli></x-dipli>
+        </x-n>
+        <x-m>
+          <x-m-pa></x-m-pa>
+          <x-m-alpha></x-m-alpha>
+        </x-m>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o2-m></x-o2-m>
+          <x-kl></x-kl>
+          <x-ii-ga className="byz--ii"></x-ii-ga>
+          <x-ly>не</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-ok></x-ok>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o></x-o>
+          <x-kl></x-kl>
+          <x-f-agem-b className="byz--f"></x-f-agem-b>
+          <x-ly>тлен</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-y></x-y>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-k></x-k>
+          <x-g-b className="byz--g"></x-g-b>
+        </x-n>
+        <x-n>
+          <x-ok></x-ok>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-e></x-e>
+          <x-ly>но</x-ly>
+        </x-n>
+        <x-n>
+          <x-k></x-k>
+        </x-n>
+        <x-n>
+          <x-i></x-i>
+        </x-n>
+        <x-m>
+          <x-m-zo-high></x-m-zo-high>
+          <x-m-nana></x-m-nana>
+        </x-m>
+        <x-n>
+          <x-i></x-i>
+          <x-ly>ро</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o></x-o>
+          <x-kl></x-kl>
+          <x-ly>ди</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-e></x-e>
+          <x-kl></x-kl>
+          <x-ly>ла</x-ly>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-ok></x-ok>
+          <x-g className="byz--g"></x-g>
+          <x-ly>Бо</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-re></x-re>
+          <x-ly>га</x-ly>
+        </x-n>
+        <x-n>
+          <x-k></x-k>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o-i-k></x-o-i-k>
+          <x-gd className="byz--g"></x-gd>
+          <x-ly>Сло</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-var></x-var>
+          <x-i></x-i>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-i></x-i>
+          <x-kl></x-kl>
+          <x-ly>во</x-ly>
+        </x-n>
+        <x-m>
+          <x-m-ke></x-m-ke>
+          <x-m-alpha-aa></x-m-alpha-aa>
+        </x-m>
+        <x-n>
+          <x-i></x-i>
+          <x-g className="byz--g"></x-g>
+          <x-ly>съ</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-i></x-i>
+          <x-kl></x-kl>
+          <x-ly>щин</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-o2></x-o2>
+          <x-anti></x-anti>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-re></x-re>
+          <x-ly>ска</x-ly>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o></x-o>
+          <x-kl></x-kl>
+          <x-ly>Бо</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-kl></x-kl>
+          <x-ly>го</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-ok></x-ok>
+          <x-psi></x-psi>
+          <x-ly>ро</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-ly>ди</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-a></x-a>
+          <x-dipli></x-dipli>
+          <x-ly>ца</x-ly>
+        </x-n>
+        <x-n>
+          <x-p></x-p>
+          <x-ly>Теб</x-ly>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-o-a-k></x-o-a-k>
+          <x-g className="byz--g"></x-g>
+          <x-ly>ве</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-re></x-re>
+          <x-ly>ли</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-k></x-k>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-ok></x-ok>
+          <x-psi></x-psi>
+          <x-ly>ча</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-a></x-a>
+          <x-kl></x-kl>
+        </x-n>
+        <x-n>
+          <x-a></x-a>
+          <x-ly>ем</x-ly>
+        </x-n>
+        <x-n>
+          <x-k></x-k>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-ok></x-ok>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-a></x-a>
+          <x-dipli></x-dipli>
+        </x-n>
+        <x-n>
+          <x-y></x-y>
+          <x-g className="byz--g"></x-g>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-a></x-a>
+        </x-n>
+        <x-n>
+          <x-ko></x-ko>
+          <x-argon className="byz--g"></x-argon>
+        </x-n>
+        <x-m>
+          <x-m-ga></x-m-ga>
+          <x-m-nana></x-m-nana>
+        </x-m>
+      </div>
+    </div>
+  )
+}
