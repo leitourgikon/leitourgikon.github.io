d94067361eb4ef37ddc5e141daaa9910d9072b7a
dani31x <176744712+dani31x@users.noreply.github.com>
2025-03-24 10:52:45 +0200

    Reword 'Save, O Lord'

--- src/chants/17.tsx
+++ src/chants/17.tsx
@@ -15,7 +15,7 @@ export default function Component() {
           <x-mel a h></x-mel>
         </x-n>
         <x-n>
-          <x-bar-s></x-bar-s>
+          <x-bar-th-s></x-bar-th-s>
           <x-o3></x-o3>
           <x-kl-b></x-kl-b>
           <x-ly>си</x-ly>
@@ -81,7 +81,7 @@ export default function Component() {
           <x-mel a h></x-mel>
         </x-n>
         <x-n>
-          <x-bar-s></x-bar-s>
+          <x-bar-th-s></x-bar-th-s>
           <x-o2></x-o2>
           <x-psi></x-psi>
           <x-yf2></x-yf2>
@@ -122,32 +122,51 @@ export default function Component() {
           <x-m-alpha></x-m-alpha>
         </x-m>
         <x-n>
-          <x-i></x-i>
+          <x-ok></x-ok>
+          <x-ly>да</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-bar-s></x-bar-s>
+          <x-p></x-p>
+          <x-kl-b></x-kl-b>
+          <x-ly>ря</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-ea></x-ea>
           <x-kl></x-kl>
+          <x-ly>вай</x-ly>
+          <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
+          <x-ly>ки</x-ly>
+        </x-n>
+        <x-n>
+          <x-o></x-o>
           <x-ly>по</x-ly>
           <x-mel a h></x-mel>
         </x-n>
         <x-n>
           <x-bar-s></x-bar-s>
-          <x-var></x-var>
-          <x-o3></x-o3>
-          <x-om-c></x-om-c>
+          <x-i></x-i>
           <x-ly>бе</x-ly>
           <x-mel a h></x-mel>
         </x-n>
         <x-n>
-          <x-i></x-i>
+          <x-k></x-k>
         </x-n>
         <x-n>
-          <x-ea></x-ea>
+          <x-i></x-i>
           <x-ly>ди</x-ly>
         </x-n>
         <x-n>
-          <x-i></x-i>
+          <x-a></x-a>
           <x-ly>на</x-ly>
         </x-n>
         <x-n>
-          <x-o></x-o>
+          <x-a></x-a>
           <x-ly>пра</x-ly>
           <x-mel a h></x-mel>
         </x-n>
@@ -158,9 +177,13 @@ export default function Component() {
         </x-n>
         <x-n>
           <x-bar-s></x-bar-s>
-          <x-o-i></x-o-i>
+          <x-i></x-i>
           <x-ly>слав</x-ly>
           <x-mel a h></x-mel>
+        </x-n>
+        <x-n>
+          <x-k></x-k>
+          <x-g className="byz--g"></x-g>
         </x-n>
         <x-n>
           <x-o></x-o>
@@ -168,20 +191,20 @@ export default function Component() {
           <x-mel a h></x-mel>
         </x-n>
         <x-n>
-          <x-i></x-i>
+          <x-a></x-a>
           <x-ly>те</x-ly>
         </x-n>
         <x-n>
-          <x-i></x-i>
+          <x-a></x-a>
           <x-ly>над</x-ly>
         </x-n>
         <x-n>
-          <x-a></x-a>
+          <x-i></x-i>
           <x-ly>вра</x-ly>
           <x-mel a h></x-mel>
         </x-n>
         <x-n>
-          <x-o></x-o>
+          <x-a></x-a>
           <x-ly>го</x-ly>
           <x-mel a h></x-mel>
         </x-n>
@@ -197,29 +220,8 @@ export default function Component() {
         </x-n>
         <x-n>
           <x-a></x-a>
-          <x-ly>те</x-ly>
-        </x-n>
-        <x-n>
-          <x-a></x-a>
-          <x-ly>да</x-ly>
-          <x-mel a h></x-mel>
-        </x-n>
-        <x-n>
-          <x-bar-s></x-bar-s>
-          <x-o></x-o>
-          <x-psi></x-psi>
-          <x-ly>ря</x-ly>
-          <x-mel a h></x-mel>
-        </x-n>
-        <x-n>
-          <x-a></x-a>
-          <x-ly>вай</x-ly>
-          <x-mel a h></x-mel>
-        </x-n>
-        <x-n>
-          <x-a></x-a>
           <x-kl></x-kl>
-          <x-ly>ки</x-ly>
+          <x-ly>те</x-ly>
         </x-n>
         <x-m>
           <x-m-pa></x-m-pa>
@@ -236,23 +238,24 @@ export default function Component() {
         </x-n>
         <x-n>
           <x-bar-s></x-bar-s>
-          <x-p></x-p>
+          <x-o2></x-o2>
+          <x-psi></x-psi>
           <x-ly>паз</x-ly>
           <x-mel a h></x-mel>
         </x-n>
         <x-n>
-          <x-e></x-e>
+          <x-a></x-a>
           <x-ly>вай</x-ly>
           <x-mel a h></x-mel>
         </x-n>
         <x-n>
-          <x-o></x-o>
+          <x-a></x-a>
           <x-kl></x-kl>
           <x-ly>ки</x-ly>
         </x-n>
         <x-n>
           <x-bar-s></x-bar-s>
-          <x-o2></x-o2>
+          <x-o></x-o>
           <x-psi></x-psi>
           <x-ly>с'Кръс</x-ly>
           <x-mel a h></x-mel>
@@ -263,18 +266,14 @@ export default function Component() {
         </x-n>
         <x-n>
           <x-a></x-a>
-          <x-kl></x-kl>
           <x-ly>Си</x-ly>
         </x-n>
         <x-n>
-          <x-bar-s></x-bar-s>
+          <x-bar-th-s></x-bar-th-s>
           <x-o></x-o>
           <x-psi></x-psi>
           <x-ly>Тво</x-ly>
           <x-mel a h></x-mel>
-        </x-n>
-        <x-n>
-          <x-a></x-a>
         </x-n>
         <x-n>
           <x-a></x-a>
